8a4e96238cd111b12e579ecb19027906fc271c31
Jordan Brown <92428577+t0mmusic@users.noreply.github.com>
2022-09-29 17:34:17 +0930

    End of Day 4

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,26 +1,53 @@
+
+var itemArray = [];
+loadCookie();
+
+function loadCookie() {
+    var arr = JSON.parse(localStorage.getItem("items"));
+    for (var i = 0; i < arr.length; i++)
+    {
+        addToList(arr[i]);
+    }
+
+}
+
+function addToArray (inputItem) {
+    itemArray.push(inputItem);
+    localStorage.setItem("items", JSON.stringify(itemArray));
+}
+
+function addToList(inputItem) {
+    var newItem = document.createElement("li");
+    newItem.classList.add("task");
+    newItem.addEventListener("click", removeElem);
+    var newContent = document.createTextNode(inputItem);
+    newItem.appendChild(newContent);
+    var currDiv = document.getElementById("ft_list");
+    currDiv.prepend(newItem);
+    addToArray(inputItem);
+}
+
+
 function addItem() {
+
     let inputItem = prompt("Please enter a todo item.", "");
     
-    if (inputItem == "") {
-        console.log("Hooray!");
+    if (!inputItem == "" || inputItem) {
+        addToList(inputItem);
     }
-    else
-    {
-        var newItem = document.createElement("li");
-        newItem.classList.add("task");
-        newItem.addEventListener("click", removeElem);
-        var newContent = document.createTextNode(inputItem);
-        newItem.appendChild(newContent);
-        var currDiv = document.getElementById("todo");
-        currDiv.prepend(newItem);
-        console.log(inputItem);
-
-    }
-
 }
 
 function removeElem() {
     let isConform = confirm("Remove this from the list?");
     if (isConform)
+    {
         this.remove();
+        var list = document.getElementsByTagName("li");
+        itemArray.length = 0;
+        for (var i = 0; i < list.length; i++)
+        {
+            itemArray.unshift(list[i].textContent);
+        }
+        localStorage.setItem("items", JSON.stringify(itemArray));
+    }
 }
